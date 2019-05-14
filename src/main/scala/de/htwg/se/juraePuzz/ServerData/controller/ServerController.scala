9667427b12785ded30f42b620b722a37503a6b42
Matthias Asche <matthiaskg.asche@gmail.com>
2019-05-14 10:51:40 +0200

    saving in db works

--- src/main/scala/de/htwg/se/juraePuzz/ServerData/controller/ServerController.scala
+++ src/main/scala/de/htwg/se/juraePuzz/ServerData/controller/ServerController.scala
@@ -6,24 +6,24 @@ import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model.{HttpRequest, _}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
-import org.mongodb.scala.{Completed, MongoClient, MongoCollection, MongoDatabase}
-import HttpMethods._
+import com.mongodb.async.client.{Observer, Subscription}
+import org.mongodb.scala.bson.collection.mutable.Document
+import org.mongodb.scala.{Completed, MongoClient, MongoCollection, MongoDatabase, SingleObservable}
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
-import scala.xml.Document
-
 
 
 class ServerController {
 
-  implicit val system = ActorSystem()
+
+  implicit val system = ActorSystem("System")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
   val client: MongoClient = MongoClient("mongodb://localhost:27017")
   val database: MongoDatabase = client.getDatabase("JuraeDB")
   val collection: MongoCollection[Document] = database.getCollection("JuraeColl")
-
 
 
   val json: String =
@@ -74,20 +74,10 @@ class ServerController {
 
 
   def server(): Unit = {
-    val serverSource = Http().bind(interface = "localhost", port = 8889)
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8888/grid"))
-
-    responseFuture
-      .onComplete {
-        case Success(res) => println(res)
-        case Failure(_)   => sys.error("something wrong")
-      }
-
-    val insertObservable: Observable[Completed] = collection.insertOne(res)
 
 
-
-
+    val serverSource = Http().bind(interface = "localhost", port = 8888)
+    val requestHandler: HttpRequest => HttpResponse = {
 
 
       case HttpRequest(GET, Uri.Path("/grid"), _, _, _) =>
@@ -96,15 +86,45 @@ class ServerController {
       case HttpRequest(GET, Uri.Path("/left"), _, _, _) =>
         HttpResponse(entity = "left")
 
-    val requestHandler: HttpRequest => HttpResponse = {
 
       case HttpRequest(GET, Uri.Path("/load"), _, _, _) => {
-        HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, json))
+        val response = collection.find().subscribe((doc: Document) => println(doc.toJson()))
+        HttpResponse(entity = response.toString)
       }
-      case HttpRequest(GET, Uri.Path("/save"), _, _, _) =>
+      case HttpRequest(GET, Uri.Path("/save"), _, _, _) => {
+        val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+          method = HttpMethods.GET,
+          uri = "http://localhost:9090/grid",
+          entity = HttpEntity(ContentTypes.`application/json`, json.toString)
+
+        ))
+        responseFuture.onComplete {
+          case Success(value) =>
+            val test: Future[String] = value.entity.toStrict(5 seconds).map(_.data.decodeString("UTF-8"))
+            test.onComplete {
+              case Success(x) => {
+                val doc: Document = Document(x)
+                val insertObservable: SingleObservable[Completed] = collection.insertOne(doc)
+
+                insertObservable.subscribe(new Observer[Completed] {
+                  override def onNext(result: Completed): Unit = println(s"onNext: $result")
+
+                  override def onError(e: Throwable): Unit = println(s"onError: $e")
+
+                  override def onComplete(): Unit = println("onComplete")
+
+                  override def onSubscribe(subscription: Subscription): Unit = print("was soll das teil hier? Steht nicht in der doku")
+                })
+              }
+              case Failure(_) => sys.error("Document creation failed")
+            }
+
+          case Failure(_) => sys.error("Request failed")
+        }
+        print(responseFuture)
         HttpResponse(entity = "saved")
 
-
+      }
       case HttpRequest(GET, Uri.Path("/right"), _, _, _) =>
         HttpResponse(entity = "right")
 
@@ -127,14 +147,6 @@ class ServerController {
         // this is equivalent to
         // connection handleWith { Flow[HttpRequest] map requestHandler }
       }).run()
-
-  }
-
-
-
-  def handleDatabase(): Unit = {
-
-
 
   }
 
